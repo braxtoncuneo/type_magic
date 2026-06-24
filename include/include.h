7162c28c6b118754afc06278e763b93dee2e9c5e
braxtoncuneo <braxton.s.cuneo@gmail.com>
2026-06-24 14:02:19 -0700

    Moved include.h to include directory

--- include/include.h
+++ include/include.h
@@ -4,19 +4,12 @@
 #include <vector>
 #include <cxxabi.h>
 
-
 #include "setup/preamble.h"
 
-
-#include "sanity_check.cpp"
-
-
 #include "setup/config.h"
-
 
 #include "container/mod.h"
 #include "context/mod.h"
 #include "core/mod.h"
-
 
 #include "setup/postamble.h"
