a739a13ef23def1bbe32aa06f1f318fc3cafbe65
braxtoncuneo <braxton.s.cuneo@gmail.com>
2026-06-30 14:27:50 -0700

    Reset core/execute.h to main

--- include/core/execute.h
+++ include/core/execute.h
@@ -28,21 +28,15 @@ struct Schedule {};
 
 struct Loop{};
 
-struct For{};
-
-struct UnorderedFor{};
-
-
-
 
 namespace impl {
 
 namespace cpu {
 
-    template<typename CONDITION>
+    template<typename CONTEXT>
     struct Loop{
 
-        template<typename CONTEXT>
+        template<typename CONDITION>
         requires std::predicate<CONDITION>
         static void loop(CONDITION c) {
             while (c()){}
@@ -53,112 +47,30 @@ namespace cpu {
     template<typename TASK>
     struct Launch {
 
-        template <typename ROOT_MODULE>
-        using SubCtx = typename context::CreateContextType <
-            ROOT_MODULE,
-            container::TypeSet<TASK>,
-            typename CONTEXT::Info::Solver
-        >::type;
-    
-         
-        template <typename ROOT_MODULE>
-        struct Recurse {
-            template<typename CONTEXT> 
-            static constexpr bool SATISFIED = SubCtx<CONTEXT>
-                ::template ComponentLookup<context::ContextInfo>
-                ::ALL_REQS_SATISFIED;
-        };
-
         template<typename CONTEXT>
         struct LaunchImpl {
 
-            template<typename... ARGS>
-            void launch(ARGS... args) {
+            void operator()() {
+                typedef typename context::CreateContextType <
+                    typename CONTEXT::Info::Root,
+                    container::TypeSet<TASK>,
+                    typename CONTEXT::Info::Solver
+                >::type Ctx;
 
-                typename SubCtx<typename CONTEXT::Info::Root>
-                       ::template ComponentLookup<storage::StorageType<storage::cpu::Global>>
-                       ::type global_fields;
-
-                SubCtx<CONTEXT> ctx(global_fields);
-                as<TASK>(ctx)(args...);
+                Ctx ctx;
 
             }
 
         };
-    
+
     };
+
 
 
 }
 
 
-namespace gpu {
-  
-    /* 
-    template<typename LAUNCH_CTX, typename TASK, typename GLOBAL, typename SHARED, typename PRIVATE, typename... ARGS>
-    __global__ void launch_helper(GLOBAL * global_fields, ARGS... args) {
-            __shared__ SHARED shared_fields;
-            PRIVATE private_fields;
-            SubCtx ctx(*global_fields,shared_fields,private_fields);
-            as<TASK>(ctx)(args);
-    }
 
-
-    template<typename TASK>
-    struct KernelLaunch {
-
-        template<typename CONTEXT>
-        struct KernelLaunchImpl {
-                
-            typedef typename context::CreateContextType <
-                typename CONTEXT::Info::Root,
-                container::TypeSet<TASK>,
-                typename CONTEXT::Info::Solver
-            >::type SubCtx;
-
-            static constexpr bool SATISFIED = SubCtx::template ComponentLookup<context::ContextInfo>::ALL_REQS_SATISFIED;
-
-            template<typename... ARGS>
-            void kernel_launch(dim3 grid_dims, dim3 block_dims, ARGS... args) {
-                typedef typename SubCtx::template ComponentLookup<storage::StorageType<storage::gpu::Global>>::type  GlobalFields;
-                typedef typename SubCtx::template ComponentLookup<storage::StorageType<storage::gpu::Shared>>::type  SharedFields;
-                typedef typename SubCtx::template ComponentLookup<storage::StorageType<storage::gpu::Private>>::type PrivateFields;
-                launch_helper<<<grid_dims,block_dims>>>(args);
-            }
-
-        };
-
-    };
-
-
-    template<typename TASK>
-    struct Launch {
-
-        template<typename CONTEXT>
-        struct KernelLaunchImpl {
-                
-            typedef typename context::CreateContextType <
-                typename CONTEXT::Info::Root,
-                container::TypeSet<TASK>,
-                typename CONTEXT::Info::Solver
-            >::type SubCtx;
-
-            static constexpr bool SATISFIED = SubCtx::template ComponentLookup<context::ContextInfo>::ALL_REQS_SATISFIED;
-
-            template<typename... ARGS>
-            void kernel_launch(ARGS... args) {
-                typedef typename SubCtx::template ComponentLookup<storage::StorageType<storage::gpu::Shared>>::type  SharedFields;
-                typedef typename SubCtx::template ComponentLookup<storage::StorageType<storage::gpu::Private>>::type PrivateFields;
-                SubCtx ctx (SharedFields,PrivateFields);
-                ctx(args...);
-            }
-
-        };
-
-    };
-    */
-
-}
 
 
 
